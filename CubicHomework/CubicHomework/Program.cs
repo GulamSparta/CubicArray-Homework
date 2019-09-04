ffeb70b65e26ff2fe51b972918565851d4aff3d0
Gulam Qasim <gqasim@spartaglobal.com>
2019-09-04 09:34:00 +0100

    second commit FIX

--- CubicHomework/CubicHomework/Program.cs
+++ CubicHomework/CubicHomework/Program.cs
@@ -10,18 +10,6 @@ namespace LabsHomeWork
     {
         static void Main(string[] args)
         {
-            object CubicArray = new cubicArray();
-            cubicArray();
-        }
-    }
-
-    class cubicArray
-    {
-        //Lab 1 : Cubic Array 10x10x10  Build a three dimensional array and populate it with numbers which are 
-        //products of the input numbers.Make the size of the cube to be 10x10x10 and your 
-        //numbers will run from 1 at(1,1,1) through 1000 at(10,10,10). Numbering starts at 1 here instead of 0.
-        public void CubicArray()
-        {
             // Create a three-dimensional array.
             int[,,] cubeArray = new int[3, 5, 4];
             cubeArray[0, 0, 0] = 1;
@@ -46,6 +34,48 @@ namespace LabsHomeWork
                 }
                 Console.WriteLine();
             }
+
+            Dictionary<string, string> GulamDictionary = new Dictionary<string, string>();
+            GulamDictionary.Add("One", "America");
+            GulamDictionary.Add("two", "England");
+            GulamDictionary.Add("three", "Russia");
+            GulamDictionary.Add("four", "Iceland");
+            GulamDictionary.Add("five", "Romaina");
+            //Console.WriteLine(myDictionary["One"]);
+            GulamDictionary.ToList().ForEach(x => Console.WriteLine(x.Value));
+
+            //Queue
+            Queue<double> myQueue = new Queue<double>();
+            myQueue.Enqueue(4.5);
+            myQueue.Enqueue(434.5);
+            myQueue.Enqueue(23.9);
+
+
+
+            myQueue.Dequeue();
+
+
+
+            //Stack
+            Stack<double> myStack = new Stack<double>();
+            myStack.Push(2.4);
+            myStack.Push(67.4);
+            myStack.Push(78.4);
+            myStack.Pop();
+            double value = myStack.Peek();
+            Console.WriteLine(value);
+
+        }
+    }
+
+    class cubicArray
+    {
+        //Lab 1 : Cubic Array 10x10x10  Build a three dimensional array and populate it with numbers which are 
+        //products of the input numbers.Make the size of the cube to be 10x10x10 and your 
+        //numbers will run from 1 at(1,1,1) through 1000 at(10,10,10). Numbering starts at 1 here instead of 0.
+        public void CubicArray()
+        {
+            
         }
     }
 }
